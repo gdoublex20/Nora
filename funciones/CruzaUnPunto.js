8b00ceac42edcf21ad9931f06ca1cc7d909cacba
AAdrianML <trabajo.aaml@gmail.com>
2020-11-21 01:01:36 -0700

    lo visual para ayudar un poco

--- funciones/CruzaUnPunto.js
+++ funciones/CruzaUnPunto.js
@@ -1,26 +1,58 @@
-function CruzaUnPunto () 
-{
 var i, j, s, x, y, PuntodeCruza;
 x=0, y=0, PuntodeCruza=0;
 var poblacionseleccionada = [];
 var poblacioncruzaunpunto = [];
+/*aparte*/
+var poblacionseleccionada2 = [];
+var CruzaUnPunto1 = [];
+var PuntoCruza = [];
+var u=0;
+
+function CruzaUnPunto () 
+{
+
 var numFilas=parseInt(document.getElementById("numeroid").value); 
 for(var f = 0; f< numFilas; f++)
 {
  for(var c = 0; c < 5; c++)
  {
     console.log("hakiando el planeta");
-    poblacionseleccionada[f] = muestra[seleccionados[f]-1]
+    poblacionseleccionada[f] = muestra[seleccionados[f]-1];
+    poblacionseleccionada2[f] = muestra[seleccionados[f]-1];
  }
 }
 
 //Select a random crossover point
 PuntoDeCruza = Math.round(Math.random() * 4) + 1  
 console.log("hakiando el planetaaaaa", PuntoDeCruza);
-
 console.log("otra población ",poblacioncruzaunpunto);
 console.log("CruzaUnPunto ",muestra);
 console.log(seleccionados);
 console.log("Población Seleccionada",poblacionseleccionada);
+
+   for(var f = 0; f< numFilas; f++)
+   {
+         
+         var si = Math.round(Math.random())>0.5 ? true : false;
+         if(si==true){
+         CruzaUnPunto1[f]=si;
+         CruzaUnPunto1[f+1]=si;
+         
+         u=Math.round(Math.random() * 4) + 1  ;
+         PuntoCruza[f]=u;
+         PuntoCruza[f+1]=u;
+         f++;
+         }else{
+         CruzaUnPunto1[f]=si;
+         CruzaUnPunto1[f+1]=si;
+         PuntoCruza[f]=0;
+         PuntoCruza[f+1]=0;
+         f++;
+         }
+
+   }
+/*ya les deje algunas cosas extras para que sea mas visual, ya solo falta la otra mitad, ustedes pueden :3 */
+
+    
 }
 
--- funciones/tabla.js
+++ funciones/tabla.js
@@ -27,17 +27,30 @@ function graficarTablaGetDocument() {
 function tabla(){
   var numColumnas=1;
   var codigoHTML="";
+  var codigoHTML2="";
   /*numero filas--SE OCUPA EJECUTAR*/
   var numFila=parseInt(document.getElementById("numeroid").value); 
 x=0;
 for (var i = 0; i < numFila; i++) {
-  codigoHTML+="<tr>";
-  x++;
-  for (var j = 0; j < numColumnas; j++) {
-    codigoHTML+="<td>"+x+"</td>"+ "<td>"+muestra[i]+"</td>" +"<td>"+suma2[i]+"</td>" +"<td>"+invalea[i]+"</td>"+"<td>"+ganador[i]+"</td>"+"<td>"+valestimado[i].toFixed(2)+"</td>"+"<td>"+sumvalestimado[i].toFixed(2)+"</td>"+"<td>"+r[i]+"</td>" +"<td>"+seleccionados[i]+"</td>" +"<!--celda-->";
-  }
-  codigoHTML+="</tr>";
+
+  codigoHTML+="<tr class=tb2>";
+
+  if(CruzaUnPunto1[i]==true){
+  codigoHTML2+="<tr class=tt>";
+}else{
+  codigoHTML2+="<tr class=ff>";
 }
+
+    codigoHTML+="<td>"+(i+1)+"</td>"+ "<td>"+muestra[i]+"</td>" +"<td>"+suma2[i]+"</td>" +"<td>"+invalea[i]+"</td>"+"<td>"+ganador[i]+"</td>"+"<td>"+valestimado[i].toFixed(2)+"</td>"+"<td>"+sumvalestimado[i].toFixed(2)+"</td>"+"<td>"+r[i]+"</td>" +"<td>"+seleccionados[i]+"</td>" +"<!--celda-->";
+    
+    codigoHTML2+="<td>"+(i+1)+"</td>"+"<td>"+poblacionseleccionada[i]+"</td>"+"<td >"+CruzaUnPunto1[i]+"</td>"+"<td >"+PuntoCruza[i]+"</td>";
+
+  codigoHTML+="</tr>";
+  codigoHTML2+="</tr>";
+}
+
+
+
 document.getElementById("tbodyid").innerHTML = codigoHTML;
 var codigoHTML="";
 codigoHTML+="<td class=si colspan=2>"+"</td>";
@@ -45,4 +58,8 @@ codigoHTML+="<td>"+promedioP2.toFixed(2)+"</td>";
 codigoHTML+="<td class=si colspan=3>"+"</td>";
 
 document.getElementById("tablaPromedio").innerHTML = codigoHTML;
+
+
+document.getElementById("tbodyid2").innerHTML = codigoHTML2;
+
 }
